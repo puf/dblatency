7df6803a7836fc23ce8e6def97d1e4c2d195c8eb
puf <frank.vanpuffelen@gmail.com>
2023-04-13 15:59:01 -0700

    Measure multiple databases for Firestore

--- index.js
+++ index.js
@@ -7,14 +7,12 @@ const sendBtn = document.getElementById('send');
 const logElm = document.getElementById('log');
 const countElm = document.getElementById('clientcount');
 const rtdbTable = document.getElementById('rtdbtable');
-
-const sentat2Elm = document.getElementById('sentat2');
-const responses2Table = document.getElementById('responsestable2');
+const firestoreTable = document.getElementById('firestoretable');
 
 import { initializeApp, getApp } from "firebase/app";
 import { getAuth, signInAnonymously, onAuthStateChanged } from "firebase/auth";
 import { getDatabase, ref, child, push, set, remove, onValue, onDisconnect, serverTimestamp } from "firebase/database";
-import { getFirestore, collection, doc, setDoc, deleteDoc, getDocs, onSnapshot } from "firebase/firestore";
+import { getFirestore, collection, doc, setDoc, deleteDoc, getDocs, onSnapshot, initializeFirestore, serverTimestamp } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyALE_zSIPfqjyJw_bIOLYNpq7kqiKsD2nc", // auth
   projectId: "dblatency", // firestore
@@ -25,6 +23,10 @@ const RTDB_URLS = {
   "asia-southeast1": "https://dblatency-asia-southeast1.asia-southeast1.firebasedatabase.app/",
   "europe-west1": "https://dblatency-europe-west1.europe-west1.firebasedatabase.app/",
 };
+const FIRESTORE_INSTANCES = {
+  "nam5": "(default)",
+  "eur3": "eur3",
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
@@ -32,9 +34,7 @@ let rtdb = getDatabase(app); // TODO: do this based on dropdown
 const firestore = getFirestore(app);
 
 let root = ref(rtdb, 'latency');
-let sendRef = child(root, "send");
-let echoRef = child(root, "echo");
-let myid, myEchoRTDBUnsub, myEchoFirestoreUnsub, mymsg, sendTimestamp;
+let myid;
 
 for (const [name, url] of Object.entries(RTDB_URLS)) {
   // Create Firebase App instance
@@ -53,6 +53,18 @@ for (const [name, url] of Object.entries(RTDB_URLS)) {
   valueCell.id = `rtdb-${name}`;
   row.appendChild(valueCell);
   rtdbTable.appendChild(row);
+}
+
+for (const [label, name] of Object.entries(FIRESTORE_INSTANCES)) {
+  const row = document.createElement('tr');
+  const nameCell = document.createElement('td');
+  nameCell.innerText = label;
+  row.appendChild(nameCell);
+  const valueCell = document.createElement('td');
+  valueCell.id = `firestore-${label}`;
+  row.appendChild(valueCell);
+  firestoreTable.appendChild(row);
+
 }
 
 // Auth and presence
@@ -91,17 +103,6 @@ const log = (msg) => {
 }
 
 sendBtn.addEventListener("click", (e) => {
-  mymsg = push(sendRef).key;
-  sendTimestamp = Date.now();
-
-  // Remove previous responses from database
-  remove(child(echoRef, myid)); // remove previous echo nodes
-  // TODO: remove previous echo docs?
-
-  // Remove previous responses from UI
-  //responsesTable.innerHTML = "";
-  responses2Table.innerHTML = "";
-
   // Measure write latency to all RTDB instances
   for (const [name, url] of Object.entries(RTDB_URLS)) {
     const db = getDatabase(getApp(name));
@@ -117,83 +118,24 @@ sendBtn.addEventListener("click", (e) => {
     });
   };
 
-  // Send ping message that other clients will echo
-  // setDoc(sendDocRef, { sender: myid, msg: mymsg, timestamp: sendTimestamp });
+  // Measure write latency to all Firestore instances
+  for (const [label, name] of Object.entries(FIRESTORE_INSTANCES)) {
+    const db = initializeFirestore(app, {}, name);
+    const myref = doc(db, "latency", myid);
+    const now = Date.now();
+    const cell = document.getElementById(`firestore-${label}`);
+    console.log(`Writing ${now} to ${myref.path}`);
+    setDoc(myref, { timestamp: now, serverTimestamp: serverTimestamp() }).then(() => {
+      console.log(`Firestore: ${Date.now()-now}ms`)
+      cell.innerText = `${Date.now()-now}ms`;
+    }).catch((e) => {
+      console.error(e);
+      log(`Error: ${e}`);
+    });
+  }
 })
 
-
-let echoCount = 0;
-onValue(sendRef, (snapshot) => {
-  if (!snapshot.exists()) return;
-  const data = snapshot.val();
-  if (data.sender !== myid && echoCount++ < 10) {
-    set(child(child(echoRef, data.sender), myid), Date.now());
-    // clear any data for our latest echo from the database)
-    remove(child(echoRef, myid));
-    log("Responded to RTDB ping from "+data.sender);
-  }
-  else {
-    //sentatElm.innerText = data.timestamp + " (" + new Date(data.timestamp) + ")";
-  }
-});
 function setMyID(newid) {
-  if (myEchoRTDBUnsub) myEchoRTDBUnsub();
-  if (myEchoFirestoreUnsub) myEchoFirestoreUnsub();
-
   myid = newid;
   myidElm.innerText = myid;
-
-  myEchoRTDBUnsub = onValue(child(echoRef, myid), (snapshot) => {
-    console.log(Date.now()+`Got response for RTDB ping with ${snapshot.size} nodes`);
-    if (!snapshot.exists()) return;
-    //responsesTable.innerHTML = "";
-    // snapshot.forEach((responseSnapshot) => {
-    //   const key = `rtdb_${responseSnapshot.key}`;
-    //   let tr = document.getElementById(key);
-    //   if (!tr) {
-    //     console.log("adding result for "+key);
-    //     const tr = document.createElement("tr");
-    //     tr.id = key;
-    //     tr.appendChild(createElementWithText("td", responseSnapshot.key));
-    //     tr.appendChild(createElementWithText("td", responseSnapshot.val()));
-    //     tr.appendChild(createElementWithText("td", (Date.now() - sendTimestamp)+"ms"));
-    //     responsesTable.appendChild(tr);
-    //   }
-    // });
-  })
-  myEchoFirestoreUnsub = onSnapshot(collection(collectionRef, myid, "echo"), (snapshot) => {
-    console.log(Date.now()+`: Got response for Firestore ping with ${snapshot.size} docs`);
-    if (snapshot.empty) return;
-    //responses2Table.innerHTML = "";
-    snapshot.docs.forEach((responseSnapshot) => {
-      const key = `fs_${responseSnapshot.id}`;
-      let tr = document.getElementById(key);
-      if (!tr) {
-        console.log("adding result for "+key);
-        const tr = document.createElement("tr");
-        tr.id = key;
-        tr.appendChild(createElementWithText("td", responseSnapshot.id));
-        tr.appendChild(createElementWithText("td", responseSnapshot.data().timestamp));
-        tr.appendChild(createElementWithText("td", (Date.now() - sendTimestamp)+"ms"));
-        responses2Table.appendChild(tr);
-      }
-    });
-  });
 }
-
-onSnapshot(sendDocRef, (snapshot) => {
-  if (!snapshot.exists()) return;
-  const data = snapshot.data();
-  if (data.sender !== myid && echoCount++ < 10) {
-    const echoDoc = doc(collectionRef, data.sender, "echo", myid);
-    setDoc(echoDoc, { timestamp: Date.now() });
-    log("Responded to Firestore ping from "+data.sender);
-    // TODO: clear any data for our latest echo from Firestore (but not fro the UI?)
-    getDocs(collection(collectionRef, myid, "echo")).then((snapshot) => {
-      snapshot.docs.forEach((doc) => { deleteDoc(doc.ref); });
-    })
-  }
-  else {
-    sentat2Elm.innerText = data.timestamp + " (" + new Date(data.timestamp) + ")";
-  }
-});
